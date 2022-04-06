6ac338de087aedf177dcc0e80871aa43454aa312
Hunter <hunterzwang@gmail.com>
2022-04-06 07:33:03 -0400

    First Commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NEXT_PUBLIC_ENVIRONMENT === "PROD";
+
 const nextConfig = {
   reactStrictMode: true,
+  basePath: isProd ? '/you-missed-my-heart' : '', 
+  assetPrefix: isProd ? '/you-missed-my-heart' : '' 
 }
 
 module.exports = nextConfig
