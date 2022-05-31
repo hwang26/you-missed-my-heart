ccbc23a1ca7f24a07c9318d73f7a3a347e4d4061
Hunter <hunterzwang@gmail.com>
2022-05-31 08:07:21 -0400

    Updated button CSS to look cuter

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,13 +1,61 @@
 
 import styles from './Form.module.scss';
+import {useState} from 'react';
 
 const Form = (props) => {
-    // We should perform some clientside validation to make sure the image is the correct type
-    // We can also style the button a bit better
+    
+   const [file, setFile] = useState(null);
+
+    // https://developer.mozilla.org/en-US/docs/Web/Media/Formats/Image_types
+    const fileTypes = [
+        "image/apng",
+        "image/bmp",
+        "image/gif",
+        "image/jpeg",
+        "image/pjpeg",
+        "image/png",
+        "image/svg+xml",
+        "image/tiff",
+        "image/webp",
+        "image/x-icon"
+    ];
+
+
+    // Used in JSX as a visual
+    let fileName="";
+
+    if(file !== null){
+        fileName = file.name;
+    };
+
+    // If the file is a valid image, return true
+    function validFileType(unknownFile) {
+        if(unknownFile !==(null || undefined)){
+            return fileTypes.includes(unknownFile.type);
+        }
+        return false;
+    }
+
+    // Checks if the file is a valid image; if valid, sets the file
+   function changeHandler(e){
+        let uploadedFile = e.target.files[0];
+        let isValid = validFileType(uploadedFile);
+
+        if(isValid){
+            setFile(uploadedFile);
+        }
+   }
+
     return(
         <form className={styles.formStyle} method="post" encType="multipart/form-data">
+            {/*Label is for the image input element*/}
             <label htmlFor="imageInput">Upload Image</label>
-            <input id="imageInput" type="file" accept="image/*" />
+            {
+                fileName !== "" ?
+                (<p>{fileName}</p>) :
+                null
+            }
+            <input id="imageInput" type="file" accept="image/*" onChange={changeHandler}/>
             <button>Submit</button>
         </form>
     );
